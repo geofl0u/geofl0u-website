d4889af2986c2ceac28d24d8d4f8165db3e20a18
Georgios <giwrgos.flour@gmail.com>
2026-09-19 12:54:40 +0200

    add about page

---
+++ src/app/components/backButton.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+import "~/styles/backButton.css";
+
+export default function BackButton() {
+  return (
+    <div className="m-auto height-auto flex flex-col gap-2 text-zinc-300">
+        <Link
+          className="animate-fade-in text-l hover:text-zinc-500"
+          href="/"
+        >
+          <span className="back-icon" />
+        </Link>
+      </div>
+  );
+}
