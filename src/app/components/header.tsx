24b22630840ba3ce168e832794d259dd1f148b95
Georgios <giwrgos.flour@gmail.com>
2026-09-19 12:54:33 +0200

    layout polishing

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,6 +1,6 @@
-export default function Header() {
+export default function Header(props: {title: string, subtitle: string}) {
   return (<header className="animate-fade-in-up mb-16">
-    <h1 className="text-text text-2xl tracking-tight justify-start">geofl0u</h1>
-    <p className="text-zinc-500 mt-1 text-sm/4 text-left">wanders through pixels and APIs</p>
+    <h1 className="text-text text-2xl tracking-tight justify-start">{props.title}</h1>
+    <p className="text-zinc-500 mt-1 text-sm/4 text-left">{props.subtitle}</p>
   </header>)
 }
