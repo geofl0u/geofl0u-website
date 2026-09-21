b305ff0475b5e81f2a32f999176708c48984c97f
Georgios <giwrgos.flour@gmail.com>
2026-09-21 18:55:24 +0200

    feat: add footer with github account

--- src/app/components/pageWrapper.tsx
+++ src/app/components/pageWrapper.tsx
@@ -1,8 +1,17 @@
-export default function PageWrapper(props: {children: React.ReactNode}) {
+import Link from "next/link";
+import "~/styles/footer.css";
+
+export default function PageWrapper(props: { children: React.ReactNode }) {
   return (
-    <main className="flex flex-col text-text">
-      <div className="container mx-auto flex max-w-3xl flex-col items-start justify-center gap-2 px-16 py-80">
+    <main className="text-text flex flex-col">
+      <div className="container mx-auto flex max-w-3xl flex-col items-start justify-center gap-2 px-16 pt-80 pb-32">
         {props.children}
+      </div>
+      <div className="footer container mx-auto flex max-w-3xl flex-col justify-center gap-2 px-16">
+        <div className="divider m-auto w-xs" />
+        <Link href={"https://github.com/geofl0u"} target="_tab">
+          <span className="git-icon pt-8" />
+        </Link>
       </div>
     </main>
   );
