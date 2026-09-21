515455fd231760168136b6524b62977e7c827968
Georgios <giwrgos.flour@gmail.com>
2026-09-21 22:33:27 +0200

    fix; button selection

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -6,9 +6,11 @@ export default function Footer() {
   return (
     <div className="footer container mx-auto flex max-w-3xl flex-col justify-center gap-2 pb-12">
       <div className="divider m-auto w-xs" />
-      <Link href={"https://github.com/geofl0u"} target="_tab">
-        <span className="git-icon pt-8" />
-      </Link>
+      <Link
+        href={"https://github.com/geofl0u"}
+        target="_tab"
+        className="git-icon pt-8"
+      />
     </div>
   );
 }
