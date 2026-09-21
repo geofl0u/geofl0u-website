d4b0c1d88aa28b39a00003aeb4d9647db003d266
Georgios <giwrgos.flour@gmail.com>
2026-09-21 18:56:26 +0200

    feat: polish projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,23 @@
+import { v4 as uuidv4 } from "uuid";
 import BackButton from "../components/backButton";
 import Header from "../components/header";
 import PageWrapper from "../components/pageWrapper";
 import ProjectCard from "../components/projectCard";
+import { PROJECTS } from "../PROJECTS";
 
 export default function ProjectsPage() {
   return (
     <PageWrapper>
       <Header title={"projects"} subtitle={"let there be code"} />
-      <div className="w-100% mb-32 grid grid-cols-2 gap-8">
-        <ProjectCard
-          url={"https://graceful-speculoos-575232.netlify.app/"}
-          title={"react wordle"}
-          desc={
-            "Wordle clone developed in react. It was a puzzle during Rethymno's treasure hunt 2026 in which I participated in the organization."
-          }
-        />
+      <div className={`w-100% just mb-32 grid grid-cols-2 gap-8`}>
+        {PROJECTS.map((p) => (
+          <ProjectCard
+            key={uuidv4()}
+            url={p.url}
+            title={p.title}
+            desc={p.desc}
+          />
+        ))}
       </div>
       <BackButton />
     </PageWrapper>
