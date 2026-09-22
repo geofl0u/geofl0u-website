41696674892083f45a7dc87abe491c10a54ba63b
Georgios <giwrgos.flour@gmail.com>
2026-09-22 17:20:56 +0200

    fix typo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,14 +6,16 @@ import BackButton from "../components/backButton";
 export default function AboutPage() {
   return (
     <PageWrapper>
-      <Header title={'Georgios Flouris'} subtitle={'Software Engineer'} />
+      <Header title={"Georgios Flouris"} subtitle={"Software Engineer"} />
       <div className="flex flex-col gap-2 text-zinc-400">
         <div className="animate-fade-in text-xl">
           Never good with about pages, but I guess I had to include one on my
           portfolio website. Here to show the coding I have done for fun in my
-          free time. Proud nerd. Video games, board games, TTRPGs, fantasy and pretty much
-          anything than can identify me as a nerd.
-          <p className="text-xs text-zinc-600 py-16 text-center">journey before destination</p>
+          free time. Proud nerd. Video games, board games, TTRPGs, fantasy and
+          pretty much anything that can identify me as a nerd.
+          <p className="py-16 text-center text-xs text-zinc-600">
+            journey before destination
+          </p>
         </div>
       </div>
       <BackButton />
