b0c319b375d4fe8958b68c604fb78269b4d2b884
Georgios <giwrgos.flour@gmail.com>
2026-09-22 22:58:35 +0200

    typo fix

--- src/app/PROJECTS.ts
+++ src/app/PROJECTS.ts
@@ -2,6 +2,6 @@ export const PROJECTS: { url: string; title: string; desc: string }[] = [
   {
     url: "https://graceful-speculoos-575232.netlify.app/",
     title: "react wordle",
-    desc: "Wordle clone developed in react. It was a puzzle during Rethymno's treasure hunt 2026 in which I participated in the organization.",
+    desc: "Wordle clone developed in react. It was a puzzle during Rethymno's treasure hunt 2026 in which I participated in the organisation.",
   },
 ];
